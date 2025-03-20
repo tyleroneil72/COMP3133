d807d8fe9f592d84871c75eac6798c15dade97ca
Tyler O'Neil <tyleroneildev@gmail.com>
2025-03-20 11:12:13 -0400

    Lab 8 Exercise 2

--- Lab08_101434244/toh-pt2/src/app/heroes/heroes.component.ts
+++ Lab08_101434244/toh-pt2/src/app/heroes/heroes.component.ts
@@ -1,27 +1,18 @@
-import {Component} from '@angular/core';
-import {
-  NgIf,
-  NgFor,
-  UpperCasePipe,
-} from '@angular/common';
-import {FormsModule} from '@angular/forms';
+import { Component } from "@angular/core";
+import { NgIf, NgFor, UpperCasePipe } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { RemoveSpacesPipe } from "../remove-spaces.pipe";
 
-import {Hero} from '../hero';
-import {HEROES} from '../mock-heroes';
+import { Hero } from "../hero";
+import { HEROES } from "../mock-heroes";
 
 @Component({
   standalone: true,
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css'],
-  imports: [
-    FormsModule,
-    NgIf,
-    NgFor,
-    UpperCasePipe,
-  ],
+  selector: "app-heroes",
+  templateUrl: "./heroes.component.html",
+  styleUrls: ["./heroes.component.css"],
+  imports: [FormsModule, NgIf, NgFor, UpperCasePipe, RemoveSpacesPipe],
 })
-
 export class HeroesComponent {
   heroes = HEROES;
   selectedHero?: Hero;
